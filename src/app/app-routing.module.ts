16b52d2b03da7013496be8b6693ef5c1a7a5ea3b
Breno1707 <piloto1707@gmail.com>
2019-02-10 18:15:12 -0200

    10-02-2019-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,17 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  { path: 'instalacao', loadChildren: './instalacao/instalacao.module#InstalacaoPageModule' }
+  {path: 'home', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'instalacao', loadChildren: './instalacao/instalacao.module#InstalacaoPageModule' },
+  { path: 'layout', loadChildren: './layout/layout.module#LayoutPageModule' },
+  { path: 'layout-header-footer', loadChildren: './layout-header-footer/layout-header-footer.module#LayoutHeaderFooterPageModule' },
+  { path: 'layout-tabs', loadChildren: './layout-tabs/layout-tabs.module#LayoutTabsPageModule' },
+  { path: 'layout-menu', loadChildren: './layout-menu/layout-menu.module#LayoutMenuPageModule' },
+  { path: 'layout-split', loadChildren: './layout-split/layout-split.module#LayoutSplitPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {title: 'Home', url: '/home', icon: 'home'},
-    {title: 'Instalação', url: '/instalacao', icon: 'home'}
+    {title: 'Instalação', url: '/instalacao', icon: 'home'},
+    {title: 'Layout', url: '/layout', icon: 'home'},
   ];
 
   constructor(
